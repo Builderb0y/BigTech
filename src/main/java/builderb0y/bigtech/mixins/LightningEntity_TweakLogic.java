279ba3738d2c5068cf33d2df559a791a138f1f9a
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2023-11-29 18:42:51 -0600

    fix tesla coils being able to produce infinite lightning.

--- src/main/java/builderb0y/bigtech/mixins/LightningEntity_TweakLogic.java
+++ src/main/java/builderb0y/bigtech/mixins/LightningEntity_TweakLogic.java
@@ -2,14 +2,19 @@ package builderb0y.bigtech.mixins;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LightningEntity;
 
 @Mixin(LightningEntity.class)
 public abstract class LightningEntity_TweakLogic extends Entity {
+
+	@Shadow private boolean cosmetic;
 
 	public LightningEntity_TweakLogic() {
 		super(null, null);
@@ -30,5 +35,15 @@ public abstract class LightningEntity_TweakLogic extends Entity {
 	@Redirect(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LightningEntity;seed:J", opcode = Opcodes.PUTFIELD))
 	private void bigtech_dontChangeSeed(LightningEntity instance, long value) {
 		//don't do anything.
+	}
+
+	/**
+	prevent infinite loops where a tesla coil can summon
+	lightning, which activates a lightning rod,
+	which pulses the tesla coil again.
+	*/
+	@Inject(method = "powerLightningRod", at = @At("HEAD"), cancellable = true)
+	private void bigtech_dontActivateLightningRodsWhenCosmetic(CallbackInfo callback) {
+		if (this.cosmetic) callback.cancel();
 	}
 }
