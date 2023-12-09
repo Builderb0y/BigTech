a2c9ba2105f6f32302bd3087a430647bd12bf0f1
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2023-12-08 18:34:38 -0600

    fix lithium compatibility with frames and catwalk stairs.

--- src/main/java/builderb0y/bigtech/mixins/EntityShapeContext_ImplementHeldItemGetter.java
+++ src/main/java/builderb0y/bigtech/mixins/EntityShapeContext_ImplementHeldItemGetter.java
@@ -1,19 +1,12 @@
 package builderb0y.bigtech.mixins;
 
-import java.util.function.Predicate;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.EntityShapeContext;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 
-import builderb0y.bigtech.BigTechMod;
 import builderb0y.bigtech.mixinterfaces.HeldItemGetter;
 
 @Mixin(EntityShapeContext.class)
@@ -21,15 +14,14 @@ public class EntityShapeContext_ImplementHeldItemGetter implements HeldItemGette
 
 	@Shadow @Final private ItemStack heldItem;
 
-	@Inject(method = "<init>(ZDLnet/minecraft/item/ItemStack;Ljava/util/function/Predicate;Lnet/minecraft/entity/Entity;)V", at = @At("RETURN"))
-	private void bigtech_sanityCheck(boolean descending, double minY, ItemStack heldItem, Predicate walkOnFluidPredicate, Entity entity, CallbackInfo ci) {
-		if (heldItem == null) {
-			BigTechMod.LOGGER.warn("Someone constructed an EntityShapeContext with a null heldItem!", new Throwable("stack trace"));
-		}
-	}
-
 	@Override
 	public ItemStack bigtech_getHeldItem() {
+		//lithium nulls out this field for no apparent reason.
+		//it doesn't save on any performance,
+		//and it breaks assumptions other mods make.
+		//https://github.com/CaffeineMC/lithium-fabric/issues/498
+		//I have an override in fabric.mod.json to disable lithium's mixin to this class,
+		//but I'm leaving this null check here just in case another mod does something equally dumb.
 		return this.heldItem != null ? this.heldItem : ItemStack.EMPTY;
 	}
 }
